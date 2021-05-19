df232329d07567f67f6a54390722580262de17c7
Caleb Denio <cjdenio44@gmail.com>
2021-05-19 16:54:57 -0400

    Initial commit

---
+++ bin/deploy.js
@@ -0,0 +1,16 @@
+const axios = require("axios").default;
+const { WebClient } = require("@slack/web-api");
+
+async function deploy() {
+  const client = new WebClient(process.env.SLACK_TOKEN);
+
+  client.chat.postMessage({
+    channel: "C015M6U6JKU",
+    text: "hmm",
+  });
+}
+
+deploy().catch((e) => {
+  console.log(`error deploying: ${e}`);
+  process.exit(1);
+});
